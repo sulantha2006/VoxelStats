b49f98e9a0ba3e4e8d0af2394e56aae0874b5609
sulantha <sulantha.s@gmail.com>
2015-05-11 12:50:09 -0400

    removed EMMA-toolbox from package control

--- emma-master/matlab/general/calpix.m
+++
@@ -1,8 +0,0 @@
-function cp = calpix(x,y)
-% CALPIX  Obsolete function - please use PIXELINDEX instead
-
-% $Id: calpix.m,v 1.5 1997-10-20 18:23:19 greg Rel $
-% $Name:  $
-
-disp ('calpix is obsolete.  Please use pixelindex instead');
-cp = pixelindex ([128 128], x, y);
--- emma-master/matlab/general/getcwd.m
+++
@@ -1,7 +0,0 @@
-function dir=getcwd;
-%
-% Function to return the current working directory.
-% Putting pwd in here makes it easier to get around compiler limitations.
-%
-
-dir=pwd;
