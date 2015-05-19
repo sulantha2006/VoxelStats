3db9a188063d2b685922fd9a4485561c3db8dce7
sulantha <sulantha.s@gmail.com>
2015-05-19 17:41:03 -0400

    Short by default

--- VoxelStatsWrite.m
+++ VoxelStatsWrite.m
@@ -1,7 +1,7 @@
 function VoxelStatsWrite( data, filename, ref_file, noOfSlices, imageType )
 switch nargin
     case 4
-        imageType = 'float';
+        imageType = 'short';
 end
 h = newimage(filename, [0 noOfSlices], ref_file, imageType);
 putimages(h, data, 1:noOfSlices);
