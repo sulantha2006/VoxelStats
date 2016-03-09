ce8b0dbbdbe8daf9e59c06924f171136528181c4
Sulantha <sulantha.s@gmail.com>
2016-03-09 08:26:54 -0500

    UI elements on dev

--- Ui/chooseVariable.m
+++ Ui/chooseVariable.m
@@ -1,6 +1,5 @@
 function chooseVariable(hObject,eventdata, type)
 handles = guidata(hObject);
-handles.c_data(2)
     if (strcmp(type,'lm'))
         str_selection = get(handles.chooseEst_lm, 'Value');
         newfields = fieldnames(handles.c_data(str_selection));
