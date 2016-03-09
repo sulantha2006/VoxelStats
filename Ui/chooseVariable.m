a5f6973c6f7f6d9bd8df25ff2c2ce4fb5668979f
Sulantha <sulantha.s@gmail.com>
2016-03-09 08:38:14 -0500

    UI elements on dev

--- Ui/chooseVariable.m
+++ Ui/chooseVariable.m
@@ -3,13 +3,13 @@ handles = guidata(hObject);
     if (strcmp(type,'lm'))
         str_selection_s = get(handles.chooseEst_lm, 'String');
         str_selection = str_selection_s{get(handles.chooseEst_lm, 'Value')};
-        newfields = fieldnames(handles.c_data.str_selection);
+        newfields = eval([fieldnames(handles.c_data.str_selection)]);
         set(handles.chooseVarName_lm, 'String', newfields);
     end
     if (strcmp(type,'glm'))
         str_selection_s = get(handles.chooseEst_glm, 'String');
         str_selection = str_selection_s{get(handles.chooseEst_glm, 'Value')};
-        newfields = fieldnames(handles.c_data.str_selection);
+        newfields = eval([fieldnames(handles.c_data.str_selection)]);
         set(handles.chooseVarName_glm, 'String', newfields);
     end
     
