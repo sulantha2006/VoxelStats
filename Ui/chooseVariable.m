1bea47aa3dd7cdbdf1eb1b97e87a6aee07a6c578
Sulantha <sulantha.s@gmail.com>
2016-03-09 08:35:54 -0500

    UI elements on dev

--- Ui/chooseVariable.m
+++ Ui/chooseVariable.m
@@ -1,12 +1,14 @@
 function chooseVariable(hObject,eventdata, type)
 handles = guidata(hObject);
     if (strcmp(type,'lm'))
-        str_selection = get(handles.chooseEst_lm, 'Value');
+        str_selection_s = get(handles.chooseEst_lm, 'String');
+        str_selection = str_selection_s{get(handles.chooseEst_lm, 'Value')};
         newfields = fieldnames(handles.c_data(str_selection));
         set(handles.chooseVarName_lm, 'String', newfields);
     end
     if (strcmp(type,'glm'))
-        str_selection = get(handles.chooseEst_glm, 'Value');
+        str_selection_s = get(handles.chooseEst_glm, 'String');
+        str_selection = str_selection_s{get(handles.chooseEst_glm, 'Value')};
         newfields = fieldnames(handles.c_data(str_selection));
         set(handles.chooseVarName_glm, 'String', newfields);
     end
