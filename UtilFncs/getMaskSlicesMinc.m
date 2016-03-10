98012f5d7454b58bb9e032aa1901aa9d8cfb08fb
Sulantha <sulantha.s@gmail.com>
2016-03-10 14:50:13 -0500

    Added functions to get voxel_dims

--- UtilFncs/getMaskSlicesMinc.m
+++ UtilFncs/getMaskSlicesMinc.m
@@ -6,7 +6,7 @@ function [mask_slices_n, mask_height, mask_width, mask_slices, voxel_dims] = get
         mask_height = getimageinfo(mask, 'ImageHeight');
         mask_width = getimageinfo(mask, 'ImageWidth');
         mask_slices_t = getimages(mask, 1:mask_slices_n);
-        voxel_dims = getimageinfo(openimage(maskFile), 'Steps');
+        voxel_dims = getimageinfo(mask, 'Steps');
     catch
         fprintf('File reading failed for : %s \nSleeping 5s before retrying...', mask_file);
         try
@@ -18,7 +18,7 @@ function [mask_slices_n, mask_height, mask_width, mask_slices, voxel_dims] = get
         mask_height = getimageinfo(mask, 'ImageHeight');
         mask_width = getimageinfo(mask, 'ImageWidth');
         mask_slices_t = getimages(mask, 1:mask_slices_n);
-        voxel_dims = getimageinfo(openimage(maskFile), 'Steps');
+        voxel_dims = getimageinfo(mask, 'Steps');
         fprintf('Done...\n');
     end
     mask_slices = mask_slices_t > 0.9;
