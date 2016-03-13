f8aefc0934500c0cf1831f0567c4837bc1323bad
Sulantha <sulantha.s@gmail.com>
2016-03-13 11:58:22 -0400

    Mod choose Var

--- Ui/chooseVariable.m
+++ Ui/chooseVariable.m
@@ -3,16 +3,20 @@ handles = guidata(hObject);
     if (strcmp(type,'lm'))
         str_selection_s = get(handles.chooseEst_lm, 'String');
         str_selection = str_selection_s{get(handles.chooseEst_lm, 'Value')};
-        intVars = eval(['handles.c_data.' str_selection]);
-        newfields = fieldnames(intVars);
-        set(handles.chooseVarName_lm, 'String', newfields);
+        if ~strcmp(str_selection, '')
+            intVars = eval(['handles.c_data.' str_selection]);
+            newfields = fieldnames(intVars);
+            set(handles.chooseVarName_lm, 'String', newfields);
+        end
     end
     if (strcmp(type,'glm'))
         str_selection_s = get(handles.chooseEst_glm, 'String');
         str_selection = str_selection_s{get(handles.chooseEst_glm, 'Value')};
-        intVars = eval(['handles.c_data.' str_selection]);
-        newfields = fieldnames(intVars);
-        set(handles.chooseVarName_glm, 'String', newfields);
+        if ~strcmp(str_selection, '')
+            intVars = eval(['handles.c_data.' str_selection]);
+            newfields = fieldnames(intVars);
+            set(handles.chooseVarName_glm, 'String', newfields);
+        end
     end
     
 
