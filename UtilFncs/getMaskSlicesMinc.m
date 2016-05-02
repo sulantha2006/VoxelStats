0ee7734c6ec2f4d5a711b4d22b03bb00c35a3798
sulantha <sulantha.s@gmail.com>
2016-05-02 10:19:43 -0400

    Moded read in functions

--- UtilFncs/getMaskSlicesMinc.m
+++ UtilFncs/getMaskSlicesMinc.m
@@ -1,25 +1,27 @@
 function [mask_slices_n, mask_height, mask_width, mask_slices, voxel_dims, slices_data] = getMaskSlicesMinc(mask_file)
     mask = [];
-    try
-        mask = openimage(mask_file);
-        mask_slices_n = getimageinfo(mask, 'NumSlices');
-        mask_height = getimageinfo(mask, 'ImageHeight');
-        mask_width = getimageinfo(mask, 'ImageWidth');
-        mask_slices_t = getimages(mask, 1:mask_slices_n);
-        voxel_dims = getimageinfo(mask, 'Steps');
-    catch
-        fprintf('File reading failed for : %s \nSleeping 5s before retrying...', mask_file);
+    for retry=1:5
         try
-            closeimage(mask);
+            mask = openimage(mask_file);
+            mask_slices_n = getimageinfo(mask, 'NumSlices');
+            mask_height = getimageinfo(mask, 'ImageHeight');
+            mask_width = getimageinfo(mask, 'ImageWidth');
+            mask_slices_t = getimages(mask, 1:mask_slices_n);
+            voxel_dims = getimageinfo(mask, 'Steps');
+            break;
+        catch
+            fprintf('File reading failed for : %s \nSleeping 5s before retrying...', mask_file);
+            try
+                closeimage(mask);
+            end
+            pause(5);
+            if retry < 5
+                continue;
+            else
+                fprintf('File reading failed and connot recover. ')
+                exit
+            end
         end
-        pause(5);
-        mask = openimage(mask_file);
-        mask_slices_n = getimageinfo(mask, 'NumSlices');
-        mask_height = getimageinfo(mask, 'ImageHeight');
-        mask_width = getimageinfo(mask, 'ImageWidth');
-        mask_slices_t = getimages(mask, 1:mask_slices_n);
-        voxel_dims = getimageinfo(mask, 'Steps');
-        fprintf('Done...\n');
     end
     mask_slices = mask_slices_t > 0.9;
     slices_data = mask_slices_t;
--- UtilFncs/getMaskSlicesNifti.m
+++ UtilFncs/getMaskSlicesNifti.m
@@ -1,25 +1,27 @@
 function [mask_slices_n, mask_height, mask_width, mask_slices, voxel_dims, slices_data] = getMaskSlicesNifti(mask_file)
     mask = [];
-    try
-        mask = load_nii(mask_file);
-        mask_slices_n = mask.hdr.dime.dim(4);
-        mask_height = mask.hdr.dime.dim(3);
-        mask_width = mask.hdr.dime.dim(2);
-        mask_slices_t = reshape(mask.img, [], mask_slices_n);
-        voxel_dims = [mask.hdr.dime.pixdim(2), mask.hdr.dime.pixdim(3), mask.hdr.dime.pixdim(4)];
-    catch
-        fprintf('File reading failed for : %s \nSleeping 5s before retrying...', mask_file);
+    for retry=1:5
         try
-            clear mask;
+            mask = load_nii(mask_file);
+            mask_slices_n = mask.hdr.dime.dim(4);
+            mask_height = mask.hdr.dime.dim(3);
+            mask_width = mask.hdr.dime.dim(2);
+            mask_slices_t = reshape(mask.img, [], mask_slices_n);
+            voxel_dims = [mask.hdr.dime.pixdim(2), mask.hdr.dime.pixdim(3), mask.hdr.dime.pixdim(4)];
+            break;
+        catch
+            fprintf('File reading failed for : %s \nSleeping 5s before retrying...', mask_file);
+            try
+                clear mask;
+            end
+            pause(5);
+            if retry < 5
+                continue;
+            else
+                fprintf('File reading failed and connot recover. ')
+                exit
+            end
         end
-        pause(5);
-        mask = load_nii(mask_file);
-        mask_slices_n = mask.hdr.dime.dim(4);
-        mask_height = mask.hdr.dime.dim(3);
-        mask_width = mask.hdr.dime.dim(2);
-        mask_slices_t = reshape(mask.img, [], mask_slices_n);
-        voxel_dims = [mask.hdr.dime.pixdim(2), mask.hdr.dime.pixdim(3), mask.hdr.dime.pixdim(4)];
-        fprintf('Done...\n');
     end
     mask_slices = mask_slices_t > 0;
     slices_data = mask_slices_t;
